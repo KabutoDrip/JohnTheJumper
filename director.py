803ad9c7cadbf0995cbaf0136093c9ee93da0dc5
Jenny <smilingjenny@gmail.com>
2022-02-05 08:58:59 -0700

    updated notes on director methods for game loop

--- director.py
+++ director.py
@@ -1,16 +1,19 @@
-#  fill in: from import 
+#  from import 
 
 class Director:
   #  A module that directs the game. 
 
   #  Attributes:
   #  is_playing (boolean): Whether or not to keep playing.
+  #  word: instance of Word class
+  #  terminal: instance of Terminal class
+  #  parachute: instance of Parachute class
   #  etc.
         
 
   def __init__(self):
     # Constructs a new Director.
-    
+   
     # Args:
     # self (Director): an instance of Director.
      
@@ -30,15 +33,16 @@ class Director:
 
 
   def _get_inputs(self):  
-    # 
-
+    # Uses a Terminal method to get the users guess 
+    
     # Args:
     # self (Director): An instance of Director.
     
     
     
   def _do_updates(self):
-     #
+    # Uses a Word method to check if the users guess is in the word
+    # Updates the display with the correctly guessed letters or cuts the cord using Parachute and Word methods
 
      # Args:
      # self (Director): An instance of Director.
@@ -46,7 +50,7 @@ class Director:
       
       
   def _do_outputs(self):
-    #
+    # Uses a Terminal method to return the updated display
     
     # Args:
     # self (Director): An instance of Director.
