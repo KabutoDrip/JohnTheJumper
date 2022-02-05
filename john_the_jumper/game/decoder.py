029e2601b2c17e8cdb0eadacf431e6136e1f3769
Mary Goff <maryrgoff@gmail.com>
2022-02-05 14:07:05 -0700

    The design for a decoder class and file

---
+++ john_the_jumper/game/decoder.py
@@ -0,0 +1,33 @@
+# Create a class called Decoder to set and decode the game word
+
+# A constructor
+#   Args: 
+#       self (decoder): An instance of Decoder.
+#
+#   Attributes: To store the privat stirng word and the privat string hidden word.
+#       self.game_word (string): The game word.
+#       self.hidden_word (string): The underscors version of the game word, to guess against.
+#       self.good_guess (boolean): If the current guess is part of the word.
+
+# A setter to set the game word from a list of potential words.
+#   Args: 
+#       self (decoder): An instance of Decoder.
+
+# A setter that sets underscores, as the new value for the hidden word, for each letter in the game word.
+#   Args: 
+#       self (decoder): An instance of Decoder.
+
+# A getter to return the vaule of the hidden word.
+#   Args: 
+#       self (decoder): An instance of Decoder.
+
+# a setter that takes the player guess, checks if the letter is in the game word, updates good guess boolean,
+# updates the hidden word at the index of the letter(s) is in the game word.
+#   Args: 
+#       self (decoder): An instance of Decoder.
+#       guess (string): A single letter from guess from terminal input.
+
+# A getter to return the good guess boolean.
+#   Args: 
+#       self (decoder): An instance of Decoder.
+
