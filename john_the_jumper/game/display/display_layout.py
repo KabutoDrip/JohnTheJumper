49b581688bdc4720fc403812f5ebf4376f579579
KabutoDrip <cammiller42003@gmail.com>
2022-02-05 16:02:04 -0500

    display_layout commit

---
+++ john_the_jumper/game/display/display_layout.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Display_game:
+    
+    def __init__(self):
+        ##initialize pygame
+        ##initialize mixer
+        print()
+    def create_surface():
+        ## create surface
+        ## load images onto surface
+        ## load initial parachute state
+        print()
+    def mixer():
+        ## loads mixer to play sounds
+        print()
+    def update_display():
+        ## update display with parachute state if player guesses incorrectly
+        print()
+    def terminal_response():
+        ## responds in the terminal based on your guess showing if it was correct or not.
+        print()
