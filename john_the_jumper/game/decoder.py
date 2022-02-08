105f02e12d5d12b434ebd1006f5b8302bd60f35d
Mary Goff <maryrgoff@gmail.com>
2022-02-08 14:33:44 -0700

    Made/constructed decoder. made setters for the attributes.

--- john_the_jumper/game/decoder.py
+++ john_the_jumper/game/decoder.py
@@ -1,21 +1,36 @@
 # Create a class called Decoder to set and decode the game word
+import random
 
+class Decoder():
 # A constructor
 #   Args: 
 #       self (decoder): An instance of Decoder.
 #
 #   Attributes: To store the privat stirng word and the privat string hidden word.
-#       self.game_word (string): The game word.
-#       self.hidden_word (string): The underscors version of the game word, to guess against.
-#       self.good_guess (boolean): If the current guess is part of the word.
+#       self._set_game_word (string): The game word.
+#       self._hidden_word (string): The underscors version of the game word, to guess against.
+#       self._good_guess (boolean): If the current guess is part of the word.
+    def __init__(self):
+        self._game_word()
+        self._hidden_word()
+        self._good_guess = False
 
 # A setter to set the game word from a list of potential words.
 #   Args: 
 #       self (decoder): An instance of Decoder.
+    def _game_word(self):
+        list = ["orange", "banana", "apple"]
+        word = random.choice(list)
+        return word
 
 # A setter that sets underscores, as the new value for the hidden word, for each letter in the game word.
 #   Args: 
 #       self (decoder): An instance of Decoder.
+    def _hidden_word(self):
+        hidden_word = ""
+        for letter in self._game_word():
+            hidden_word += "_ "
+        return hidden_word 
 
 # a setter that takes the player guess, checks if the letter is in the game word, updates good guess boolean,
 # updates the hidden word at the index of the letter(s) is in the game word.
@@ -31,3 +46,4 @@
 #   Args: 
 #       self (decoder): An instance of Decoder.
 
+instance = Decoder()
