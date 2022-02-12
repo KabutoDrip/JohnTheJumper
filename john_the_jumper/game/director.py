0042c7457131947394e38825ca0b04be4efdd150
Jenny <smilingjenny@gmail.com>
2022-02-12 10:23:25 -0700

    deleted the do_updates method

--- john_the_jumper/game/director.py
+++ john_the_jumper/game/director.py
@@ -32,20 +32,10 @@ class Director:
     
     self._terminal.write_text(self._decoder.get_hidden_word)
     while self._is_playing:
-        self._get_inputs()
         self._do_updates()
         self._do_outputs()
 
-
-  def _get_inputs(self):  
-    # Uses a Terminal method to get the users guess? 
     
-    # Args:
-    # self (Director): An instance of Director.
-    
-    guess = self._terminal.read_a_character("\nGuess a letter [a-z]: ")
-    return guess
-      
   def _do_updates(self):
     # Uses a decoder method to check if the users guess is in the decoder
     # Updates the display with the correctly guessed letters or cuts the cord using display and decoder methods
